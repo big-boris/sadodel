f8de5848bea4480193a1113ecb2d2ef634a6786b
big-boris <b.g.demchenko@yandex.ru>
2016-05-22 14:40:02 +0300

    Добавлена валидация форм в тч с animate-css

--- builds/dev/js/app.js
+++ builds/dev/js/app.js
@@ -3,35 +3,40 @@ $(document).ready(function(){
     $(".phonemask").mask("+7(999) 999-9999");
 
     $('.frm__agronom').submit(function(event) {
-        console.log('Hey!');
         var name_frm_agronom = $("#name_frm_agronom");
         var phone_frm_agronom = $("#phone_frm_agronom");
         if(!$(name_frm_agronom).val() || !$(phone_frm_agronom).val()) {
-            console.log($(name_frm_agronom).val());
             $(name_frm_agronom).addClass("alert");
             $(phone_frm_agronom).addClass("alert");
+            $(name_frm_agronom).addClass("animated shake");
+            $(phone_frm_agronom).addClass("animated shake");
             setTimeout(function() {
-                $(name_frm_agronom).removeClass("alert"),
-                $(phone_frm_agronom).removeClass("alert")
+                $(name_frm_agronom).removeClass("alert");
+                $(phone_frm_agronom).removeClass("alert");
+                $(name_frm_agronom).removeClass("animated shake");
+                $(phone_frm_agronom).removeClass("animated shake");
             },3000);
+            event.preventDefault();
         };
-        event.preventDefault();
     });
 
+
     $('.frm__present').submit(function(event) {
-        console.log('Hey!');
         var name_frm_present = $("#name_frm_present");
         var phone_frm_present = $("#phone_frm_present");
         if(!$(name_frm_present).val() || !$(phone_frm_present).val()) {
-            console.log($(name_frm_present).val());
             $(name_frm_present).addClass("alert");
             $(phone_frm_present).addClass("alert");
+            $(name_frm_present).addClass("animated shake");
+            $(phone_frm_present).addClass("animated shake");
             setTimeout(function() {
-                $(name_frm_present).removeClass("alert"),
-                $(phone_frm_present).removeClass("alert")
+                $(name_frm_present).removeClass("alert");
+                $(phone_frm_present).removeClass("alert");
+                $(name_frm_present).removeClass("animated shake");
+                $(phone_frm_present).removeClass("animated shake");
             },3000);
+            event.preventDefault();
         };
-        event.preventDefault();
     });
 
     function myScrollTo(o) {
